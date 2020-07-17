23264703b82ed472e15dacc1b4883aea2725cffe
Guthen <33220603+Guthen@users.noreply.github.com>
2020-07-17 15:07:17 +0200

    🗺 Maps: renamed

---
+++ maps/B03.lua
@@ -0,0 +1,12 @@
+local P, W, _, B, D, DC, C, S = TILE_PLAYER, TILE_WALL_A, TILE_VOID, TILE_BUTTON, TILE_DOOR, TILE_DOOR_CLOSE, TILE_CUBE, TILE_SPOT
+
+return {
+	{ W, W, W, W, W, W, W, },
+	{ W, _, _, _, W, W, W, },
+	{ W, P, C, _,DC, S, W, },
+	{ W, _, _, _, W, W, W, },
+	{ W, D, B, W, W, W, W, },
+	{ W, _, C, _, D, S, W, },
+	{ W, W, W, W, W, W, W, },
+	high_score = 19,
+}
