2f323e90f76cddbd7746ad8ee84e7cb3ac4fdf89
Guthen <33220603+Guthen@users.noreply.github.com>
2020-07-17 18:27:37 +0200

    🗺 Maps: 2 new maps

---
+++ maps/B07.lua
@@ -0,0 +1,19 @@
+local P, W, _, B, D, DC, C, S = TILE_PLAYER, TILE_WALL_A, TILE_VOID, TILE_BUTTON, TILE_DOOR, TILE_DOOR_CLOSE, TILE_CUBE, TILE_SPOT
+
+return {
+	{ W, W, W, W, W, W, W, W, W, },
+	{ W, S, _, _, D, _, _, S, W, },
+	{ W, _, W, B, W, _, W, W, W, },
+	{ W, _, _, _, D, _, W, W, W, },
+	{ W, W, W,DC, W, _, W, W, W, },
+	{ W, _, C, S, _, B, _, _, W, },
+	{ W, _, W,DC, W, _, W, _, W, },
+	{ W, _, W, C, W, P, W, _, W, },
+	{ W, _, W, _, W, C, W, _, W, },
+	{ W, _, _, _, _, _, _, _, W, },
+	{ W, W, W, W, W, W, W, W, W, },
+	high_score = 85,
+	options = {
+		scale = .8,
+	}
+}
---
+++ maps/B08.lua
@@ -0,0 +1,21 @@
+local P, W, _, B, D, DC, C, S = TILE_PLAYER, TILE_WALL_A, TILE_VOID, TILE_BUTTON, TILE_DOOR, TILE_DOOR_CLOSE, TILE_CUBE, TILE_SPOT
+
+return {
+    { W, W, W, W, W, W, W, W, W, W, W, W, W, },
+    { W, P, _, _, _, _, S, _, _, _, _, _, W, },
+    { W, _, W, W, W, W, _, W, W, W, W, _, W, },
+    { W, _, W, _, _, _, D, _, C, _, W, _, W, },
+    { W, _, W, C, _, _, _, _, _, _, W, _, W, },
+    { W, _, W, _, _, W, _, W, _, _, W, _, W, },
+    { W, S, _,DC, _, _, B, _, _,DC, _, S, W, },
+    { W, _, W, _, _, W, _, W, _, _, W, _, W, },
+    { W, _, W, _, _, _, _, _, _, C, W, _, W, },
+    { W, _, W, _, C, _, D, _, _, _, W, _, W, },
+    { W, _, W, W, W, W, _, W, W, W, W, _, W, },
+    { W, _, _, _, _, _, S, _, _, _, _, _, W, },
+    { W, W, W, W, W, W, W, W, W, W, W, W, W, },
+    options = {
+        scale = .65,
+    },
+    high_score = 102,
+}
